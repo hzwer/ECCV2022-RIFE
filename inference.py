d8a7543a7bdeb51a3d3f01734143dc4eb50b85ab
hzwer <598460606@163.com>
2020-11-12 23:40:10 +0800

    Add argparse

--- inference.py
+++ inference.py
@@ -1,24 +1,26 @@
 import cv2
 import torch
+import argparse
 from model.RIFE import Model
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+parser = argparse.ArgumentParser(description='Interpolation for a pair of images')
+parser.add_argument('--img', dest='img', nargs=2)
+args = parser.parse_args()
+
+assert(len(args.img) == 2)
 
 model = Model()
 model.load_model('./train_log')
 model.eval()
 model.device()
-
-img0 = cv2.imread('0.png')
-img1 = cv2.imread('1.png')
-
+img0 = cv2.imread(args.img[0])
+img1 = cv2.imread(args.img[1])
 h, w, _ = img0.shape
-
 img0 = torch.tensor(img0.transpose(2, 0, 1)).to(device) / 255.
 img1 = torch.tensor(img1.transpose(2, 0, 1)).to(device) / 255.
-
 imgs = torch.cat((img0, img1), 0).float()
-
 with torch.no_grad():
     res = model.inference(imgs.unsqueeze(0)) * 255
-cv2.imwrite('out.png', res[0].numpy().transpose(1, 2, 0))
+cv2.imwrite('output.png', res[0].numpy().transpose(1, 2, 0))
