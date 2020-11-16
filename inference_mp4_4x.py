61a48ef61c182e2e61c88f7570cb90429fab6eff
hzwer <598460606@163.com>
2020-11-16 17:37:50 +0800

    Fix bugs

--- inference_mp4_4x.py
+++ inference_mp4_4x.py
@@ -60,7 +60,8 @@ while success:
         p = (F.interpolate(I0, (16, 16), mode='bilinear', align_corners=False)
              - F.interpolate(I1, (16, 16), mode='bilinear', align_corners=False)).abs().mean()
         if p < 1e-3 and args.skip:
-            print("Warning: Your video has {} static frames, skipping them may change the duration of the generated video.".format(cnt))
+            if cnt % 10 == 0:
+                print("Warning: Your video has {} static frames, skipping them may change the duration of the generated video.".format(cnt))
             cnt += 1
             pbar.update(1)
             continue
