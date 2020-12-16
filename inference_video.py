857e8b7842e18c6e30a08e1c868eb38baedc391b
hzwer <598460606@163.com>
2020-12-16 23:04:07 +0800

    Update inference_video.py

--- inference_video.py
+++ inference_video.py
@@ -172,8 +172,8 @@ while True:
     I0 = I1
     I1 = torch.from_numpy(np.transpose(frame, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
     I1 = F.pad(I1, padding)
-    diff = (F.interpolate(I0, (ph // 16, pw // 16), mode='bilinear', align_corners=False)
-         - F.interpolate(I1, (ph // 16, pw // 16), mode='bilinear', align_corners=False)).abs()
+    diff = (F.interpolate(I0, (16, 16), mode='bilinear', align_corners=False)
+         - F.interpolate(I1, (16, 16), mode='bilinear', align_corners=False)).abs()
     if diff.max() < 2e-3 and args.skip:
         if skip_frame % 100 == 0:
             print("Warning: Your video has {} static frames, skipping them may change the duration of the generated video.".format(skip_frame))
