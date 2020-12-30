85a3eafcb583ced4d75fa612e3af9829b7621543
hzwer <598460606@163.com>
2020-12-30 23:42:55 +0800

    Update inference_img.py

--- inference_img.py
+++ inference_img.py
@@ -8,8 +8,8 @@ import warnings
 warnings.filterwarnings("ignore")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.set_grad_enabled(False)
 if torch.cuda.is_available():
-    torch.set_grad_enabled(False)
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = True
 
