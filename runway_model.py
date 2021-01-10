ae59682e1e9c379d2de01335830229f4b299b0aa
ak9250 <ahsen.khaliq@gmail.com>
2021-01-10 16:24:54 -0500

    try in

--- runway_model.py
+++ runway_model.py
@@ -8,11 +8,20 @@ from inference_img import imgint
 from PIL import Image
 import cv2
 
-
-@runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),'target': runway.image(description='input image to be translated'),'amount': runway.number(min=0, max=100, default=0)}, outputs={'image': runway.image(description='output image containing the translated result')})
+sample_inputs= {
+    'source_imgs': runway.image(description='input image to be translated'),
+    'target': runway.image(description='input image to be translated'),
+    'amount': runway.number(min=0, max=100, default=0)
+}
+@runway.command('translate', inputs=sample_inputs, outputs={'image': runway.image(description='output image containing the translated result')})
 def translate(learn, inputs):
     listimg, h, w = imgint(inputs['source_imgs'], inputs['target'])
-    inputs['amount']['max'] = len(listimg)
+    sample_inputs= {
+    'source_imgs': runway.image(description='input image to be translated'),
+    'target': runway.image(description='input image to be translated'),
+    'amount': runway.number(min=0, max=len(listimg), default=0)
+    }
+    inputs=sample_inputs
     i = inputs['amount']
     cvimg = (listimg[i][0] * 255).byte().cpu().numpy().transpose(1, 2, 0)[:h, :w]
     img = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
