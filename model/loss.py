26378496972d1922abefaf585a179fa7a4a84d85
hzwer <598460606@163.com>
2021-04-21 12:03:28 +0800

    Update loss.py

--- model/loss.py
+++ model/loss.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
