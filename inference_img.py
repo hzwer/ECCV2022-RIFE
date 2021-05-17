d649798a31b81b9a6313e6ae71f277bab8abcce6
hzwer <598460606@163.com>
2021-05-17 16:34:09 +0800

    Update inference_img.py

--- inference_img.py
+++ inference_img.py
@@ -3,7 +3,6 @@ import cv2
 import torch
 import argparse
 from torch.nn import functional as F
-from model.RIFE_HDv2 import Model
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -19,10 +18,26 @@ parser.add_argument('--exp', default=4, type=int)
 parser.add_argument('--ratio', default=0, type=float, help='inference ratio between two images with 0 - 1 range')
 parser.add_argument('--rthreshold', default=0.02, type=float, help='returns image when actual ratio falls in given range threshold')
 parser.add_argument('--rmaxcycles', default=8, type=int, help='limit max number of bisectional cycles')
+parser.add_argument('--model', dest='modelDir', type=str, default='train_log', help='directory with trained model files')
+
 args = parser.parse_args()
 
-model = Model()
-model.load_model(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'train_log'), -1)
+try:
+    try:
+        from model.RIFE_HDv2 import Model
+        model = Model()
+        model.load_model(args.modelDir, -1)
+        print("Loaded v2.x HD model.")
+    except:
+        from train_log.RIFE_HDv3 import Model
+        model = Model()
+        model.load_model(args.modelDir, -1)
+        print("Loaded v3.x HD model.")
+except:
+    from model.RIFE_HD import Model
+    model = Model()
+    model.load_model(args.modelDir, -1)
+    print("Loaded v1.x HD model")
 model.eval()
 model.device()
 
