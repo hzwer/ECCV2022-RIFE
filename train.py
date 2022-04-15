852112e47f3a14cf06f02fce95b6338a20c8ead4
hzwer <598460606@163.com>
2022-04-15 11:30:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ def train(model, local_rank):
             timestep = timestep.to(device, non_blocking=True)
             imgs = data_gpu[:, :6]
             gt = data_gpu[:, 6:9]
-            learning_rate = get_learning_rate(step) / args.world_size
+            learning_rate = get_learning_rate(step) * args.world_size / 4
             pred, info = model.update(imgs, gt, learning_rate, training=True) # pass timestep if you are training RIFEm
             train_time_interval = time.time() - time_stamp
             time_stamp = time.time()
